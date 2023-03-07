54ec75bba72ba8001bd5574ad5699c7ac77e722f
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-03-07 14:05:41 +0100

    :sparkles: Added BUILDING controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_130239) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_130418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bibles", force: :cascade do |t|
     t.text "content"
     t.integer "party_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "buildings", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
