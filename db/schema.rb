b29ab0aacfc2d1e01c799a309bce56f6ccdb081c
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-03-07 13:36:56 +0100

    :sparkles: Added Party model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_123148) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_123545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_123148) do
     t.boolean "used"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "parties", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.bigint "universe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["universe_id"], name: "index_parties_on_universe_id"
+    t.index ["user_id"], name: "index_parties_on_user_id"
   end
 
   create_table "players", force: :cascade do |t|
@@ -65,4 +75,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_123148) do
   end
 
   add_foreign_key "creatures", "universes"
+  add_foreign_key "parties", "universes"
+  add_foreign_key "parties", "users"
 end
