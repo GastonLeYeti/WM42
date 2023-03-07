53be8faa06bcb38f28e52f16798b9c2ee6e7534d
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-03-07 12:32:30 +0100

    :sparkles: Added Player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_165248) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_112558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "username"
+    t.integer "party_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
