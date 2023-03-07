9201fbbd71e870c9043f47ed05af93c4529c979c
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-03-07 18:52:11 +0100

    ⚙️ Creat seeds for [USERS - UNIVERSES - RACES - CREATURES]

---
+++ db/creature_seeds.rb
@@ -0,0 +1,812 @@
+p "#############################################"
+p "#############################################"
+p "CREATURE SEEDS"
+p "#############################################"
+p "#############################################"
+
+p "Destroying all creatures ..."
+Creatures.destroy_all
+
+p "Creating creatures..."
+
+creature1 = Creature.new(
+  :name                => "Sanglier",
+  :lvl                 => 5,
+  :universe_id         => metaId,
+)
+creature1.save!
+p "creature Sanglier created!"
+
+creature2 = Creature.new(
+  :name                => "Loup",
+  :lvl                 => 5,
+  :universe_id         => metaId,
+)
+creature2.save!
+p "creature Loup created!"
+
+creature3 = Creature.new(
+  :name                => "Cerf",
+  :lvl                 => 2,
+  :universe_id         => metaId,
+)
+creature3.save!
+p "creature Cerf created!"
+
+creature4 = Creature.new(
+  :name                => "Lion",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature4.save!
+p "creature Lion created!"
+
+creature5 = Creature.new(
+  :name                => "Tigre",
+  :lvl                 => 8,
+  :universe_id         => metaId,
+)
+creature5.save!
+p "creature Tigre created!"
+
+creature6 = Creature.new(
+  :name                => "Gorille",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature6.save!
+p "creature Gorille created!"
+
+creature7 = Creature.new(
+  :name                => "Chauve-souris",
+  :lvl                 => 1,
+  :universe_id         => metaId,
+)
+creature7.save!
+p "creature Chauve-souris created!"
+
+creature8 = Creature.new(
+  :name                => "Aigle",
+  :lvl                 => 3,
+  :universe_id         => metaId,
+)
+creature8.save!
+p "creature Aigle created!"
+
+creature9 = Creature.new(
+  :name                => "Poule",
+  :lvl                 => 1,
+  :universe_id         => metaId,
+)
+creature9.save!
+p "creature Poule created!"
+
+creature10 = Creature.new(
+  :name                => "Poulet",
+  :lvl                 => 1,
+  :universe_id         => metaId,
+)
+creature10.save!
+p "creature Poulet created!"
+
+creature11 = Creature.new(
+  :name                => "Vache",
+  :lvl                 => 2,
+  :universe_id         => metaId,
+)
+creature11.save!
+p "creature Vache created!"
+
+creature12 = Creature.new(
+  :name                => "Cheval",
+  :lvl                 => 3,
+  :universe_id         => metaId,
+)
+creature12.save!
+p "creature Cheval created!"
+
+creature13 = Creature.new(
+  :name                => "Chien",
+  :lvl                 => 2,
+  :universe_id         => metaId,
+)
+creature13.save!
+p "creature Chien created!"
+
+creature14 = Creature.new(
+  :name                => "Chat",
+  :lvl                 => 3,
+  :universe_id         => metaId,
+)
+creature14.save!
+p "creature Chat created!"
+
+creature15 = Creature.new(
+  :name                => "Serpent",
+  :lvl                 => 8,
+  :universe_id         => metaId,
+)
+creature15.save!
+p "creature Serpent created!"
+
+creature16 = Creature.new(
+  :name                => "Souris",
+  :lvl                 => 1,
+  :universe_id         => metaId,
+)
+creature16.save!
+p "creature Souris created!"
+
+creature17 = Creature.new(
+  :name                => "Lapin",
+  :lvl                 => 1,
+  :universe_id         => metaId,
+)
+creature17.save!
+p "creature Lapin created!"
+
+creature18 = Creature.new(
+  :name                => "Hibou",
+  :lvl                 => 5,
+  :universe_id         => metaId,
+)
+creature18.save!
+p "creature Hibou created!"
+
+
+creature19 = Creature.new(
+  :name                => "Chimère",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature19.save!
+p "creature Chimère created!"
+
+creature20 = Creature.new(
+  :name                => "Centaure",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature20.save!
+p "creature Centaure created!"
+
+creature21 = Creature.new(
+  :name                => "Harpie",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature21.save!
+p "creature Harpie created!"
+
+creature22 = Creature.new(
+  :name                => "Griffon",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature22.save!
+p "creature Griffon created!"
+
+creature23 = Creature.new(
+  :name                => "Licorne",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature23.save!
+p "creature Licorne created!"
+
+creature24 = Creature.new(
+  :name                => "Minotaure",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature24.save!
+p "creature Minotaure created!"
+
+creature25 = Creature.new(
+  :name                => "Dragon",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature25.save!
+p "creature Dragon created!"
+
+creature26 = Creature.new(
+  :name                => "Hippogriffe",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature26.save!
+p "creature Hippogriffe created!"
+
+creature27 = Creature.new(
+  :name                => "Gorgone",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature27.save!
+p "creature Gorgone created!"
+
+creature28 = Creature.new(
+  :name                => "Sphinx",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature28.save!
+p "creature Sphinx created!"
+
+creature29 = Creature.new(
+  :name                => "Phénix",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature29.save!
+p "creature Phénix created!"
+
+creature30 = Creature.new(
+  :name                => "Basilic",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature30.save!
+p "creature Basilic created!"
+
+creature31 = Creature.new(
+  :name                => "Sirène",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature31.save!
+p "creature Sirène created!"
+
+creature32 = Creature.new(
+  :name                => "Kraken",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature32.save!
+p "creature Kraken created!"
+
+creature33 = Creature.new(
+  :name                => "Cerbère",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature33.save!
+p "creature Cerbère created!"
+
+creature34 = Creature.new(
+  :name                => "Cyclope",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature34.save!
+p "creature Cyclope created!"
+
+creature35 = Creature.new(
+  :name                => "Hydre",
+  :lvl                 => 10,
+  :universe_id         => metaId,
+)
+creature35.save!
+p "creature Hydre created!"
+
+creature36 = Creature.new(
+  :name                => "Le chient de Niko",
+  :lvl                 => 42,
+  :universe_id         => metaId,
+)
+creature36.save!
+p "creature Le chient de Niko created!"
+
+creature37 = Creature.new(
+  :name                => "Le sphynx de Max",
+  :lvl                 => 42,
+  :universe_id         => metaId,
+)
+creature37.save!
+p "creature Le sphynx de Max created!"
+
+creature38 = Creature.new(
+  :name                => "La chouette de Gaëtan",
+  :lvl                 => 42,
+  :universe_id         => metaId,
+)
+creature38.save!
+p "creature La chouette de Gaëtan created!"
+
+creature39 = Creature.new(
+  :name                => "La baleine de Louise",
+  :lvl                 => 42,
+  :universe_id         => metaId,
+)
+creature39.save!
+p "creature La Baleine de Louise created!"
+
+creature40 = Creature.new(
+  :name                => "La loutre harmoniciste de JJ",
+  :lvl                 => 42,
+  :universe_id         => metaId,
+)
+creature40.save!
+p "creature La loutre harmoniciste de JJ created!"
+
+# \\\\\\\//////////\\\\\\\ #
+
+####################
+p "Creating creatures for fantasy universe..."
+####################
+# Fantasy :
+#   Dragons
+#   Licornes
+#   Griffons
+#   Fées
+#   Gobelins
+#   Ogres
+#   Minotaures
+#   Hydres
+#   Harpies
+#   Elfes noirs
+
+creature41 = Creature.new(
+  :name                => "dragon",
+  :lvl                 => 10,
+  :universe_id         => fantasyId,
+)
+creature41.save!
+p "creature Dragon created!"
+
+creature42 = Creature.new(
+  :name                => "licorne",
+  :lvl                 => 6,
+  :universe_id         => fantasyId,
+)
+creature42.save!
+p "creature Licorne created!"
+
+creature43 = Creature.new(
+  :name                => "griffon",
+  :lvl                 => 8,
+  :universe_id         => fantasyId,
+)
+creature43.save!
+p "creature Griffon created!"
+
+creature44 = Creature.new(
+  :name                => "fée",
+  :lvl                 => 3,
+  :universe_id         => fantasyId,
+)
+creature44.save!
+p "creature Fée created!"
+
+creature45 = Creature.new(
+  :name                => "gobelin",
+  :lvl                 => 2,
+  :universe_id         => fantasyId,
+)
+creature45.save!
+p "creature Gobelin created!"
+
+creature46 = Creature.new(
+  :name                => "ogre",
+  :lvl                 => 4,
+  :universe_id         => fantasyId,
+)
+creature46.save!
+p "creature Ogre created!"
+
+creature47 = Creature.new(
+  :name                => "minotaure",
+  :lvl                 => 5,
+  :universe_id         => fantasyId,
+)
+creature47.save!
+p "creature Minotaure created!"
+
+creature48 = Creature.new(
+  :name                => "hydre",
+  :lvl                 => 7,
+  :universe_id         => fantasyId,
+)
+creature48.save!
+p "creature Hydre created!"
+
+creature49 = Creature.new(
+  :name                => "harpie",
+  :lvl                 => 3,
+  :universe_id         => fantasyId,
+)
+creature49.save!
+p "creature Harpie created!"
+
+creature50 = Creature.new(
+  :name                => "elfe noir",
+  :lvl                 => 6,
+  :universe_id         => fantasyId,
+)
+creature50.save!
+p "creature Elfe noir created!"
+
+#######################
+p "Creating creatures for science fiction universe..."
+#######################
+# Sci-fi :
+#   Robots
+#   Extraterrestres
+#   Cyborgs
+#   Androides
+#   Hologrammes
+#   Parasites de l'espace
+#   Créatures cristallines
+#   Réplicants
+#   Intelligences artificielles
+#   Formes de vie amorphes
+
+creature51 = Creature.new(
+  :name                => "robot",
+  :lvl                 => 3,
+  :universe_id         => scienceFictionId,
+)
+creature51.save!
+p "creature Robot created!"
+
+creature52 = Creature.new(
+  :name                => "extraterrestre",
+  :lvl                 => 5,
+  :universe_id         => scienceFictionId,
+)
+creature52.save!
+p "creature Extraterrestre created!"
+
+creature53 = Creature.new(
+  :name                => "cyborg",
+  :lvl                 => 4,
+  :universe_id         => scienceFictionId,
+)
+creature53.save!
+p "creature Cyborg created!"
+
+creature54 = Creature.new(
+  :name                => "androïde",
+  :lvl                 => 4,
+  :universe_id         => scienceFictionId,
+)
+creature54.save!
+p "creature Androïde created!"
+
+creature55 = Creature.new(
+  :name                => "hologramme",
+  :lvl                 => 1,
+  :universe_id         => scienceFictionId,
+)
+creature55.save!
+p "creature Hologramme created!"
+
+creature56 = Creature.new(
+  :name                => "parasite de l'espace",
+  :lvl                 => 6,
+  :universe_id         => scienceFictionId,
+)
+creature56.save!
+p "creature Parasite de l'espace created!"
+
+creature57 = Creature.new(
+  :name                => "créature cristalline",
+  :lvl                 => 2,
+  :universe_id         => scienceFictionId,
+)
+creature57.save!
+p "creature Créature cristalline created!"
+
+creature58 = Creature.new(
+  :name                => "réplicant",
+  :lvl                 => 5,
+  :universe_id         => scienceFictionId,
+)
+creature58.save!
+p "creature Réplicant created!"
+
+creature59 = Creature.new(
+  :name                => "intelligence artificielle",
+  :lvl                 => 4,
+  :universe_id         => scienceFictionId,
+)
+creature59.save!
+p "creature Intelligence artificielle created!"
+
+creature60 = Creature.new(
+  :name                => "forme de vie amorphe",
+  :lvl                 => 1,
+  :universe_id         => scienceFictionId,
+)
+creature60.save!
+p "creature Forme de vie amorphe created!"
+
+#######################
+p "Creating creatures for modern universe..."
+#######################
+# Modern :
+#   Chiens
+#   Chats
+#   Serpents
+#   Araignées
+#   Rat
+#   Taupes
+#   Moustiques
+#   Fourmis
+#   Cafards
+#   Pigeons
+
+creature61 = Creature.new(
+  :name                => "chien",
+  :lvl                 => 1,
+  :universe_id         => modernId,
+)
+creature61.save!
+p "creature Chien created!"
+
+creature62 = Creature.new(
+  :name                => "chat",
+  :lvl                 => 1,
+  :universe_id         => modernId,
+)
+creature62.save!
+p "creature Chat created!"
+
+creature63 = Creature.new(
+  :name                => "serpent",
+  :lvl                 => 2,
+  :universe_id         => modernId,
+)
+creature63.save!
+p "creature Serpent created!"
+
+creature64 = Creature.new(
+  :name                => "araignée",
+  :lvl                 => 2,
+  :universe_id         => modernId,
+)
+creature64.save!
+p "creature Araignée created!"
+
+creature65 = Creature.new(
+  :name                => "rat",
+  :lvl                 => 1,
+  :universe_id         => modernId,
+)
+creature65.save!
+p "creature Rat created!"
+
+creature66 = Creature.new(
+  :name                => "Taupe",
+  :lvl                 => 1,
+  :universe_id         => modernId,
+)
+creature66.save!
+p "creature Taupe created!"
+
+creature67 = Creature.new(
+  :name                => "loup",
+  :lvl                 => 2,
+  :universe_id         => modernId,
+)
+creature67.save!
+p "creature Loup created!"
+
+creature68 = Creature.new(
+  :name                => "ours",
+  :lvl                 => 8,
+  :universe_id         => modernId,
+)
+creature68.save!
+p "creature Ours created!"
+
+creature69 = Creature.new(
+  :name                => "Pigeons",
+  :lvl                 => 5,
+  :universe_id         => modernId,
+)
+creature69.save!
+p "creature Pigeons created!"
+
+creature70 = Creature.new(
+  :name                => "Cafards",
+  :lvl                 => 6,
+  :universe_id         => modernId,
+)
+creature70.save!
+p "creature Cafards created!"
+
+#######################
+p "Creating creatures for Historical universe..."
+#######################
+# Historical :
+#   Lions
+#   Tigres
+#   Éléphants
+#   Loups
+#   Hippopotames
+#   Rhinocéros
+#   Crocodiles
+#   Vautours
+#   Renards
+#   Sangliers
+
+creature71 = Creature.new(
+  :name                => "Lions",
+  :lvl                 => 5,
+  :universe_id         => historicalId,
+)
+creature71.save!
+p "creature Lions created!"
+
+creature72 = Creature.new(
+  :name                => "Tigres",
+  :lvl                 => 6,
+  :universe_id         => historicalId,
+)
+creature72.save!
+p "creature Tigres created!"
+
+creature73 = Creature.new(
+  :name                => "Éléphants",
+  :lvl                 => 10,
+  :universe_id         => historicalId,
+)
+creature73.save!
+p "creature Éléphants created!"
+
+creature74 = Creature.new(
+  :name                => "Loups",
+  :lvl                 => 3,
+  :universe_id         => historicalId,
+)
+creature74.save!
+p "creature Loups created!"
+
+creature75 = Creature.new(
+  :name                => "Hippopotames",
+  :lvl                 => 8,
+  :universe_id         => historicalId,
+)
+creature75.save!
+p "creature Hippopotames created!"
+
+creature76 = Creature.new(
+  :name                => "Rhinocéros",
+  :lvl                 => 7,
+  :universe_id         => historicalId,
+)
+creature76.save!
+p "creature Rhinocéros created!"
+
+creature77 = Creature.new(
+  :name                => "Crocodiles",
+  :lvl                 => 4,
+  :universe_id         => historicalId,
+)
+creature77.save!
+p "creature Crocodiles created!"
+
+creature78 = Creature.new(
+  :name                => "Vautours",
+  :lvl                 => 2,
+  :universe_id         => historicalId,
+)
+creature78.save!
+p "creature Vautours created!"
+
+creature79 = Creature.new(
+  :name                => "Renards",
+  :lvl                 => 1,
+  :universe_id         => historicalId,
+)
+creature79.save!
+p "creature Renards created!"
+
+creature80 = Creature.new(
+  :name                => "Sangliers",
+  :lvl                 => 8,
+  :universe_id         => historicalId,
+)
+creature80.save!
+p "creature Sangliers created!"
+
+#######################
+p "Creating creatures for Post-apo universe..."
+#######################
+# Post-apo :
+#   Rats mutants
+#   Scorpions géants
+#   Araignées mutantes
+#   Sangsues géantes
+#   Ours zombies
+#   Coyotes mutés
+#   Vautours mutants
+#   Hyènes irradiées
+#   Cafards géants
+#   Chiens de l'enfer.
+
+creature81 = Creature.new(
+  :name                => "Rats mutants",
+  :lvl                 => 3,
+  :universe_id         => postApocalypticId,
+)
+creature81.save!
+p "creature Rats mutants created!"
+
+creature82 = Creature.new(
+  :name                => "Scorpions géants",
+  :lvl                 => 5,
+  :universe_id         => postApocalypticId,
+)
+creature82.save!
+p "creature Scorpions géants created!"
+
+creature83 = Creature.new(
+  :name                => "Araignées mutantes",
+  :lvl                 => 4,
+  :universe_id         => postApocalypticId,
+)
+creature83.save!
+p "creature Araignées mutantes created!"
+
+creature84 = Creature.new(
+  :name                => "Sangsues géantes",
+  :lvl                 => 6,
+  :universe_id         => postApocalypticId,
+)
+creature84.save!
+p "creature Sangsues géantes created!"
+
+creature85 = Creature.new(
+  :name                => "Ours zombies",
+  :lvl                 => 8,
+  :universe_id         => postApocalypticId,
+)
+creature85.save!
+p "creature Ours zombies created!"
+
+creature86 = Creature.new(
+  :name                => "Coyotes mutés",
+  :lvl                 => 7,
+  :universe_id         => postApocalypticId,
+)
+creature86.save!
+p "creature Coyotes mutés created!"
+
+creature87 = Creature.new(
+  :name                => "Vautours mutants",
+  :lvl                 => 3,
+  :universe_id         => postApocalypticId,
+)
+creature87.save!
+p "creature Vautours mutants created!"
+
+creature88 = Creature.new(
+  :name                => "Hyènes irradiées",
+  :lvl                 => 6,
+  :universe_id         => postApocalypticId,
+)
+creature88.save!
+p "creature Hyènes irradiées created!"
+
+creature89 = Creature.new(
+  :name                => "Cafards géants",
+  :lvl                 => 6,
+  :universe_id         => postApocalypticId,
+)
+creature89.save!
+p "creature Cafards géants created!"
+
+creature90 = Creature.new(
+  :name                => "Chiens de l'enfer",
+  :lvl                 => 10,
+  :universe_id         => postApocalypticId,
+)
+creature90.save!
+p "creature Chiens de l'enfer created!"
+
+#######################
+
+p "Finished! #{Creature.count} creatures created!"
