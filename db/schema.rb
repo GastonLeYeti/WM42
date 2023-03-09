5d1fd71b695d6e1857693cf29631df586b470de9
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-03-09 19:03:53 +0100

    :wrench: Changed Armory table column : type -> kind

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_232437) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_232438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_232437) do
   end
 
   create_table "armories", force: :cascade do |t|
-    t.string "type"
+    t.string "kind"
     t.string "weapon_range"
     t.bigint "universe_id", null: false
     t.datetime "created_at", null: false
