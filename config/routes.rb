0ffc498a2a3e9f10b52c479522bd7607249756b7
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-03-09 11:07:57 +0100

    :spark: Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :parties, only: %i[show new create destroy]
+  resources :parties, only: %i[show new create destroy] do
+    resources :players, only: %i[index show]
+    resources :maps, only: %i[index show]
+    resources :pnjs, only: %i[index show]
+    resources :weapons, only: %i[index show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
