2c61fc38933d5717f5e5e542500768e68b187f50
Louise Plihon Dumont <louiseplihondumont@gmail.com>
2023-03-10 15:48:21 +0100

    je sais pas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-<<<<<<< HEAD
   root to: "pages#dashboard"
-=======
-  root to: "pages#home"
->>>>>>> 80aeb655c95bc72a02635d06941abf1ccd78fff0
+
   resources :parties, only: %i[show new create destroy] do
     resources :players, only: %i[index show]
     resources :maps, only: %i[index show]
