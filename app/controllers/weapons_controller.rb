fcc0af6d365f11d50c094bdc015602a98c0c52b6
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2023-03-10 15:47:23 +0100

    💩 je ne sais pas

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,2 +1,33 @@
 class WeaponsController < ApplicationController
+
+
+
+  def index
+    @weapons = Weapon.all
+  end
+
+  def show
+    @weapon = Weapon.find(params[:id])
+  end
+
+  def new
+    @weapon = Weapon.new
+  end
+
+  def create
+    @weapon = Weapon.new(params[:weapon])
+    if @weapon.save
+      redirect_to @weapon, :notice => "Successfully created weapon."
+    else
+      render :action => 'new'
+    end
+  end
+
+  private
+
+  def weapon_params
+    params.require(:weapon).permit(:name, :level, :rarity, :price, :lootable, :description)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "pages#home"
+  root "dashboard_pages#pnjs"
   resources :parties, only: %i[show new create destroy] do
     resources :players, only: %i[index show]
     resources :maps, only: %i[index show]
