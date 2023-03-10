2a8fe38a0cf56d0ebdebd7b3e093bac041449de7
maximedidry <maximedidry@gmail.com>
2023-03-10 13:40:01 +0100

    🏗️  working on routes

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,11 @@
 class MapsController < ApplicationController
+
+  def index
+    @maps = Map.all
+  end
+
+  def show
+    @map = Map.find(params[:id])
+  end
+  
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,12 @@
 class PlayersController < ApplicationController
+
+  def index
+    @players = Player.all
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  
 end
