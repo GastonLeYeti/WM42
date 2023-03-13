205f1ff6a7445646a3ea7951fc28e77186cc3108
maximedidry <maximedidry@gmail.com>
2023-03-13 18:05:51 +0100

    ✨🎨 sorted creatures by universe including meta + did cosmetics on creatures index

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,2 +1,18 @@
 class CreaturesController < ApplicationController
+
+  def index
+    @creatures = Creature.all
+    @party = Party.find(params[:party_id])
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description, :lvl, :universe_id)
+  end
+
 end
--- app/models/pnj.rb
+++ app/models/pnj.rb
@@ -10,11 +10,4 @@ class Pnj < ApplicationRecord
   validates :lvl, presence: true
   validates :status, presence: true
 
-include PgSearch::Model
-  pg_search_scope :search_by_name_and_job,
-  against: [ :name, :job ],
-  using: {
-    tsearch: { prefix: true }
-  }
-
 end
