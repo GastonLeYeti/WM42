36c63370dd491ae5066fe2a98f5761c9d4e1ecb5
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-03-15 18:10:35 +0100

    :art: formulaire en 3 parties avec fausse animation fade

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="form"
+export default class extends Controller {
+  static targets = ["section1", "section2", "section3", "formBtn1", "formBtn2"]
+  connect() {
+    console.log('eeyeyey')
+  }
+
+  hideSection1() {
+    this.section1Target.classList.add('fade')
+    setTimeout(() => {
+      this.section1Target.classList.add('d-none')
+      this.section2Target.classList.remove('d-none')
+    }, 1500);
+  }
+
+  hideSection2() {
+    this.section2Target.classList.add('fade')
+    setTimeout(() => {
+      this.section2Target.classList.add('d-none')
+      this.section3Target.classList.remove('d-none')
+    }, 1500);
+  }
+}
