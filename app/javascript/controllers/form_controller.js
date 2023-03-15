a59f0c724db356c4f33a33b05077c6c13a1048fa
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-03-15 23:05:00 +0100

    :art: formulaire en 3 steps, pas pu réorganiser step 1

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["section1", "section2", "section3", "formBtn1", "formBtn2"]
   connect() {
-    console.log('eeyeyey')
+    // console.log('eeyeyey')
   }
 
   hideSection1() {
@@ -12,7 +12,7 @@ export default class extends Controller {
     setTimeout(() => {
       this.section1Target.classList.add('d-none')
       this.section2Target.classList.remove('d-none')
-    }, 1500);
+    }, 700);
   }
 
   hideSection2() {
@@ -20,6 +20,6 @@ export default class extends Controller {
     setTimeout(() => {
       this.section2Target.classList.add('d-none')
       this.section3Target.classList.remove('d-none')
-    }, 1500);
+    }, 700);
   }
 }
