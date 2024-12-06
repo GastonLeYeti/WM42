6b7ff674fd1ac54ee6fdbdbb01b6bcc5d8be0530
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2024-12-06 01:35:05 +0100

    ⚙️ Ajout de Turbo Rails et amélioration de la syntaxe dans application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,10 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
+import "@hotwired/turbo-rails";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
